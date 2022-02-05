9d49285d85596fc0eee1860033b323ccbd56dd6d
Sourcepoint <carmelo@sourcepoint.com>
2022-02-05 15:52:56 +0100

    Updates

---
+++ precache-manifest.37be48055ed2bd3918c638e1bdb2e5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d7a1e60126b43b5070b45675e44205",
+    "url": "/tokenisation/index.html"
+  },
+  {
+    "revision": "21bb4f6b873580f7d917",
+    "url": "/tokenisation/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "80559f290db690b96c1a",
+    "url": "/tokenisation/static/js/2.b49883a4.chunk.js"
+  },
+  {
+    "revision": "21bb4f6b873580f7d917",
+    "url": "/tokenisation/static/js/main.73557808.chunk.js"
+  },
+  {
+    "revision": "bce13f7b3c7665aa943c",
+    "url": "/tokenisation/static/js/runtime-main.e95b9154.js"
+  }
+]);
